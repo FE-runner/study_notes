460f150ca5debf98ee5472db3aca1cef2ca4029d
liyang1 <liyang1@domob.cn>
2017-08-15 18:44:13 +0800

    20150909

--- js/20170515-数组去重.js
+++ js/20170515-数组去重.js
@@ -1,52 +1,51 @@
 var target = [1, 2, 34, 52, 65, 2, 1, 88, 32, 56, 99, 10, 23, 88, 54, 65, 77];
-
 // Array.prototype.form(new Set(target))
-
-// json方法 
-(function () {
-    const json = {};
-    target.forEach((item) => {
-        json[item] = -1;
-    });
-    const result = [];
-    Object.keys(json).forEach((item) => {
-        result.push(Number(item));
-    });
-    console.log(result);
+// json方法
+(function() {
+  const json = {};
+  target.forEach((item) => {
+    json[item] = -1;
+  });
+  const result = [];
+  Object.keys(json).forEach((item) => {
+    result.push(Number(item));
+  });
+  console.log(result);
 })();
 // from set
-(function () {
-    const result = Array.from(new Set(target));
-    console.log(result);
+(function() {
+  const result = Array.from(new Set(target));
+  console.log(result);
 })();
 // sort+splice
-(function () {
-    target.sort();
-    for (let i = 0; i < target.length; i++) {
-        if (i !== 0 && target[i - 1] === target[i]) {
-            target.splice(i - 1, 1);
-            i--;
-        }
+(function() {
+  target.sort();
+  for (let i = 0; i < target.length; i++) {
+    if (i !== 0 && target[i - 1] === target[i]) {
+      target.splice(i - 1, 1);
+      i--;
     }
-    console.log(target);
+  }
+  console.log(target);
 })();
 var target = [1, 2, 34, 52, 65, 2, 1, 88, 32, 56, 99, 10, 23, 88, 54, 65, 77];
 // findInArr
-(function () {
-    function findInArr (item, arr) {
-        for (let i = 0; i < arr.length; i++) {
-            if (item === arr[i]) {
-                return true;
-            }
-        }
-        return false;
+(function() {
+  function findInArr(item, arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (item === arr[i]) {
+        return true;
+      }
     }
-    const result = [];
-    for (let i = 0; i < target.length; i++) {
-        if (!findInArr(target[i], result)) {
-            result.push(target[i]);
-        }
+    return false;
+  }
+
+  const result = [];
+  for (let i = 0; i < target.length; i++) {
+    if (!findInArr(target[i], result)) {
+      result.push(target[i]);
     }
-    console.log(result);
+  }
+  console.log(result);
 })()
 ;
